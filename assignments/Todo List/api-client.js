1f1f9e2fbf7407631f2c2ec80607c8d06df64ebe
Paul Daniël <vanschevikhoven.pd@gmail.com>
2022-11-23 12:42:58 +0100

    1e entry

--- assignments/Todo List/api-client.js
+++ assignments/Todo List/api-client.js
@@ -1,3 +1,39 @@
 const baseUrl = new URL("http://localhost:3000/");
 
+const fetchTodo = async () => {
+  const response = await fetch(baseUrl, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const json = await response.json();
+  return json;
+  //   console.log(json);
+};
 
+const createTodo = async () => {
+  const data = { description: todoInput.value, done: false };
+
+  const response = await fetch(baseUrl, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  const json = await response.json();
+  return json;
+  //   console.log(json);
+};
+
+//DELETE DATA
+const deleteTodo = async (id) => {
+  const response = await fetch(baseUrl + id, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: Json.stringify({ id: id }),
+  });
+};
--- assignments/Todo List/script.js
+++ assignments/Todo List/script.js
@@ -1,48 +1,51 @@
-//Selectors
-const addInput = document.querySelector(".add-input");
-const addButton = document.querySelector(".add-btn");
+//  Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-btn");
 const todoList = document.querySelector(".todo-list");
+const deleteButton = document.querySelector(".fa-trash");
 
-//Even Listeners
-addButton.addEventListener("click", addTodo);
+//  Event Listeners
+todoButton.addEventListener("click", createTodo);
+todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
 
-//Functions
+// Functions
 function addTodo(event) {
+  // Prevent form from submitting
   event.preventDefault();
-
+  // Todo DIV
   const todoDiv = document.createElement("div");
   todoDiv.classList.add("todo");
-
-  const completeBtn = document.createElement("button");
-  completeBtn.innerHTML = '<i class="fas fa-check"></i>';
-  completeBtn.classList.add("complete-btn");
-  todoDiv.appendChild(completeBtn);
-
+  // create LI
   const newTodo = document.createElement("li");
-  newTodo.innerText = addInput.value;
+  newTodo.innerText = todoInput.value;
   newTodo.classList.add("todo-item");
   todoDiv.appendChild(newTodo);
-
-  const trashBtn = document.createElement("button");
-  trashBtn.innerHTML = '<i class="fas fa-trash"></i>';
-  trashBtn.classList.add("trash-btn");
-  todoDiv.appendChild(trashBtn);
-
+  //  CHECK MARK BUTTON
+  const completeButton = document.createElement("button");
+  completeButton.innerHTML = '<i class="fas fa-check"></i>';
+  completeButton.classList.add("complete-btn");
+  todoDiv.appendChild(completeButton);
+  //  TRASH BUTTON
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+  // APPEND TO LIST
   todoList.appendChild(todoDiv);
-  addInput.value = "";
+  // Clear todo input value
+  todoInput.value = "";
 }
 
 function deleteCheck(e) {
   const item = e.target;
-
+  //  Delete todo
   if (item.classList[0] === "trash-btn") {
     const todo = item.parentElement;
-    todo.classList.add("fall");
-    todo.addEventListener("transitionend", function () {
-      todo.remove();
-    });
+    todo.remove();
   }
+
+  // Checkmark
   if (item.classList[0] === "complete-btn") {
     const todo = item.parentElement;
     todo.classList.toggle("completed");
